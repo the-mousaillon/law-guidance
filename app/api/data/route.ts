e513e988c7cc7fdb5cca8a55bb6c9b1a79ff587e
Nicolas Morlier <nmorlier22@gmail.com>
2024-10-22 18:18:34 +0200

    Store graph on vtx blob

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -3,10 +3,13 @@
 
 import { NextRequest, NextResponse } from "next/server"
 import { D_TEXT, parseDagAndLeafsFromText } from "../../lib/graph"
+import { put, list } from '@vercel/blob';
 
 const DagStore: any = {
-    dag: D_TEXT
+    dag: null
 }
+
+const DAG_BLOB_PATH = "/dags/dag.txt"
 
 export async function POST(request: NextRequest){
     if (false && (process.env.MODIF_KEY || "1234") !== request.headers.get("MODIF_KEY")){
@@ -20,6 +23,7 @@ export async function POST(request: NextRequest){
         catch (e) {
             return NextResponse.json({message: "Error parsing DAG"}, {status: 400})
         }
+        const blob = await put(DAG_BLOB_PATH, dag.dag, { access: 'public' });
         DagStore["dag"] = dag.dag
     } 
     return NextResponse.json({message: "Success"})
@@ -27,6 +31,19 @@ export async function POST(request: NextRequest){
 
 export async function GET() {
     let dag = DagStore.dag
+    if (!dag) {
+        try {
+            let blobs = await list({
+                prefix: '/dags'
+            })
+
+            let new_dag = await (await fetch(blobs.blobs[0].downloadUrl)).text()
+            DagStore.dag = new_dag
+        }
+        catch (e) {
+            return NextResponse.json({message: "Error fetching DAG"}, {status: 400})
+        }
+    }
     let res = {
         dag: dag
     }
